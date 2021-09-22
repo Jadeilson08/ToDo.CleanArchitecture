acb6456ba890e843a826ab42d768a4f37d88142a
Jadeilson Leonardo Trizotti Rodrigues <jadeilson08@gmail.com>
2021-09-22 07:38:15 -0400

    camadas

---
+++ ToDo.Infra.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ToDo.Infra.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ ToDo.Infra.IoC/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ToDo.Infra.IoC
+{
+    public class Class1
+    {
+    }
+}
